3d8edfdc6b5b7b72192ec2746b59bc780de3c53e
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-05-09 21:25:12 +0300

    Add ui plugins
    Add tabs
    Add statusbar
    remove oil plugin

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,37 @@
+return {
+{
+   'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons'
+  }
+},
+  {
+    "arsham/arshamiser.nvim",
+    dependencies = {
+      "arsham/arshlib.nvim",
+      "famiu/feline.nvim",
+      "rebelot/heirline.nvim",
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+      require("arshamiser.feliniser")
+      _G.custom_foldtext = require("arshamiser.folding").foldtext
+      vim.opt.foldtext = "v:lua.custom_foldtext()"
+      vim.api.nvim_set_option("tabline", [[%{%v:lua.require("arshamiser.tabline").draw()%}]])
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+
+      -- Move to previous/next
+      map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+      map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+      -- Re-order to previous/next
+      map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+      map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+      -- Pin/unpin buffer
+      map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+      -- Close buffer
+      map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+    end,
+  }
+}
