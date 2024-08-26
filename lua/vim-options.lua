cd5ac80223a0e9a5a568d4868c195ae0e6ce0b3c
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2024-08-26 17:54:51 +0300

    Add jk instead of ESC

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -34,5 +34,6 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('i', 'jk', '<ESC>', {})
 vim.wo.number = true
 
