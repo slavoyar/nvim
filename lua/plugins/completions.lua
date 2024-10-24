920ce547d7c7944553c7b9d90d59a0eb47b53c15
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2024-10-24 21:31:40 +0300

    Add key bindings for codeium

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,7 +1,16 @@
 return {
   {
     'Exafunction/codeium.vim',
-    event = 'BufEnter'
+    event = 'BufEnter',
+    config = function()
+      vim.g.codeium_no_map_tab = 1;
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
   },
   {
     "hrsh7th/nvim-cmp",
@@ -54,6 +63,7 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "codeium" },
           { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
