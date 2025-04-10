7a21aa68bbd26ac33214060f7a5cf97405e1bf3f
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2025-04-10 23:21:23 +0300

    Add some neccessary treesitters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
+				ensure_installed = { "lua", "vim", "javascript", "typescript", "tsx", "jsdoc" },
 				auto_install = true,
 				highlight = {
 					enable = true,
